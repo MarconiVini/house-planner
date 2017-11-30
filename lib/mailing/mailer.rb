3f2056bdc9918bf24118fd52f9fceb2e4dabffb6
--global <msantini@ciandt.com>
2017-11-30 17:26:28 -0200

    Ajustes para funcionar o email

--- lib/mailing/mailer.rb
+++ lib/mailing/mailer.rb
@@ -9,7 +9,7 @@ class Mailer
   end
   
   def deliver_mails
-    @@config["users"]["email"].each do |email|
+    $config["users"]["email"].each do |email|
       mail = build_mail(email)
       send(mail)
     end
@@ -17,9 +17,9 @@ class Mailer
   
   private
   def build_mail(email)
-    from = Email.new(email: @@config["mail_sender"])
+    from = Email.new(email: $config["mail_sender"])
     to = Email.new(email: email)
-    subject = Time.now.strftime("%d/%m/%Y") + " " + @@config["mail_subject"]
+    subject = Time.now.strftime("%d/%m/%Y") + " " + $config["mail_subject"]
     content = Content.new(type: 'text/plain', value: @content)
     mail = Mail.new(from, subject, to, content)   
   end
